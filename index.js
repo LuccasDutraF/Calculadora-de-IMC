77119e4d690003eb2c2b909a67e64ed8d36b88da
Luccas Dutra <luccas.ferreyro@gmail.com>
2023-08-04 17:34:20 -0300

    Atualização de Responsividade

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function Calcular() {
+    var peso = window.document.getElementById('peso')
+    var altura = window.document.getElementById('altura')
+    var res = window.document.getElementById('res')
+    var peso1 = Number(peso.value)
+    var altura1 = Number(altura.value)
+    var imc = peso1 / (altura1*altura1)
 
+    res.innerHTML = (`O seu <strong>IMC</strong> ficou em ${imc}`) 
+
+   if(imc < 17){
+    res2.innerHTML = ("<strong>Muito abaixo do peso</strong><br><br><br>")
+    res2.innerHTML += ("<strong>@Copyright LDev</strong>")
+}else if(imc <= 18.5){
+    res2.innerHTML = ("<strong>Abaixo do peso</strong><br><br><br>")
+    res2.innerHTML += ("<strong>@Copyright LDev</strong>")
+}else if(imc <= 25){
+    res2.innerHTML = ("<strong>Peso normal</strong><br><br><br>")
+    res2.innerHTML += ("<strong>@Copyright LDev</strong>")
+}else if(imc <= 30){
+    res2.innerHTML = ("<strong>Acima do peso</strong><br><br><br>")
+    res2.innerHTML += ("<strong>@Copyright LDev</strong>")
+}else if(imc <= 35){
+    res2.innerHTML = ("<strong>Obesidade I</strong><br><br><br>")
+    res2.innerHTML += ("<strong>@Copyright LDev</strong>")
+}else if(imc <= 40){
+    res2.innerHTML = ("<strong>Obesidade II</strong><br><br><br>")
+    res2.innerHTML += ("<strong>@Copyright LDev</strong>")
+}else{
+    res2.innerHTML = ("<strong>Obesidade III</strong><br><br><br>")
+    res2.innerHTML += ("<strong>@Copyright LDev</strong>")
+}
+
+res2.style.textAlign = 'center'
+res.style.textAlign = 'center'
+}
