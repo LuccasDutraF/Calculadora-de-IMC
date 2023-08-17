6977b191944b5a9afea2e541a0b188c77af7b595
Luccas Dutra <luccas.ferreyro@gmail.com>
2023-08-17 13:42:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if(peso.value.length == 0){
     res2.innerHTML = ("<strong>Obesidade III</strong>")
     res3.innerHTML = ("<strong>@Copyright LDev</strong>")
 }
-res3.style.color = "#00e9ff"
+res3.style.color = "black"
 res3.style.textAlign = 'center'
 res2.style.textAlign = 'center'
 res.style.textAlign = 'center'
